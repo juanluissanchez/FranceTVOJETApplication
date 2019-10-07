8e2d9348c593948df23f73c174664f0a463b999c
Juan Luis <sanchez.juanluis@gmail.com>
2019-10-07 17:04:10 +0200

    Cleaning

--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -4,37 +4,20 @@
  * The Universal Permissive License (UPL), Version 1.0
  */
 /*
- * Dashboard viewmodel code
+ * Your dashboard ViewModel code goes here
  */
-define(['knockout','ojs/ojdatagrid','ojs/ojcollectiondatagriddatasource'],
- function(ko, $) {
+define(['knockout','ojs/ojarraydataprovider','text!../data/chartdata.json','ojs/ojchart',],
+ function(ko, ArrayDataProvider, data, $) {
 
     function DashboardViewModel() {
       var self = this;
       // Below are a set of the ViewModel methods invoked by the oj-module component.
       // Please reference the oj-module jsDoc for additional information.
-
+      
       /* toggle button variables */
       this.stackValue = ko.observable('off');
       this.orientationValue = ko.observable('vertical');
-
-      /* Data source*/
-      self.url = 'http://localhost:3000/employees';
-
-      self.collection = new oj.Collection(null, {
-          model: new oj.Model.extend({
-              idAttribute: 'id',
-              urlRoot: self.url}),
-          url: self.url
-      });
-      
-      self.dataSource = new oj.CollectionDataGridDataSource(
-         self.collection, {
-            rowHeader: 'id',
-            columns: ['FIRST_NAME', 'LAST_NAME', 'HIRE_DATE', 'SALARY']
-         });
-      
-      //this.dataProvider = new ArrayDataProvider(JSON.parse(data), {keyAttributes: 'id'});
+      this.dataProvider = new ArrayDataProvider(JSON.parse(data), {keyAttributes: 'id'});
       
       /**
        * Optional ViewModel method invoked after the View is inserted into the
